297d893c83827c5ade11b9776806a91fd4b89692
Nicodemus E Allen-Tonar <nicodemus26@gmail.com>
2018-01-29 19:09:40 -0800

    A little faster spread

--- color_drops.py
+++ color_drops.py
@@ -9,21 +9,28 @@ client = opc.Client('localhost:7890')
 
 pixels = [ (0,0,0) ] * numLEDs
 while True:
+    # Let the colors run
     new_pixels = list(pixels)
     for i in range(numLEDs):
         r, g, b = 0, 0, 0
-        for offset, mul in [(-2, .20), (-1, .6), (0, .19), (1, .2), (2, -.20)]:
+        for offset, mul in [(-2, .05), (-2, .15), (-1, .25), (0, .4), (1, .1), (2, .03)]:
             j = (numLEDs+offset+i) % numLEDs
             r = r + pixels[j][0]*mul
             g = g + pixels[j][1]*mul
             b = b + pixels[j][2]*mul
-        new_pixels[i] = min(255, r), min(255, g), min(255, b)
-    drop_spot = random.randint(0, numLEDs)
-    drop_color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
-    for i in range(drop_spot, drop_spot+3):
-        i = i % numLEDs
-        new_pixels[i] = drop_color
+        new_pixels[i] = max(4, min(255, r)), max(4, min(255, g)), max(4, min(255, b))
+
+    if(random.randint(0,3) > 0):
+        # Add a new drop of color...
+        drop_spot = random.randint(0, numLEDs)
+        drop_color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
+        for offset, mul in [(0, .25), (1, .5), (2, .75), (3, .5), (4, .25)]:
+            i = (offset + drop_spot) % numLEDs
+            color = []
+            for c in 0, 1, 2:
+                color.append(drop_color[c]*mul + new_pixels[i][c]*(1-mul))
+            new_pixels[i] = tuple(color)
     pixels = new_pixels
     discretized= [(int(r), int(g), int(b)) for (r, g, b) in pixels]
     client.put_pixels(discretized)
-    time.sleep(1)
+    time.sleep(1/10.)
