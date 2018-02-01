a6a47669ea4ad96b7c6698dd79e73159100ac074
Nicodemus E Allen-Tonar <nicodemus26@gmail.com>
2018-02-01 02:15:30 -0800

    Fix up rotate

--- rotate.py
+++ rotate.py
@@ -4,24 +4,24 @@
 
 import opc, time, random
 
-numLEDs = 256
+numLEDs = 512
 client = opc.Client('localhost:7890')
 
 colors = []
-for i in range(0, 256):
+for i in range(0, 255):
     r = i
     g = 0
-    b = 256-i
+    b = 255-i
     colors.append((r,g,b))
-for i in range(0, 256):
+for i in range(0, 255):
     g = i
     b = 0
-    r = 256-i
+    r = 255-i
     colors.append((r,g,b))
-for i in range(0, 256):
+for i in range(0, 255):
     b = i
     r = 0
-    g = 256-i
+    g = 255-i
     colors.append((r,g,b))
 while True:
     for x in colors:
