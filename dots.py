af9a55c2d4fcc729ff28b093c55ed572e9ddc900
Nicodemus E Allen-Tonar <nicodemus26@gmail.com>
2018-02-01 02:14:02 -0800

    8 channels for dots

--- dots.py
+++ dots.py
@@ -4,22 +4,21 @@
 
 import opc, time, random
 
-num_leds = 256
+num_leds = 512
 client = opc.Client('localhost:7890')
 
 yellow = 51
 purple = 57
 purple_rocket = 0
 while True:
-    if (255 + purple) % 256 == (256 + yellow) % 256:
-        yellow = (255 + yellow) % 256
+    if (num_leds + purple + 1) % num_leds == (num_leds + yellow) % num_leds:
+        yellow = (num_leds + yellow - 1) % num_leds
     else:
         if random.randint(0, 10) > (8 if (purple_rocket > 0) else 4):
-            yellow = (255 + yellow) % 256
+            yellow = (num_leds + yellow - 1) % num_leds
         else:
-            purple = (255 + purple) % 256
+            purple = (num_leds + purple - 1) % num_leds
     pixels = [(0, 0, 0)] * num_leds
-    pixels[purple] = (128, 0, 128)
     if purple_rocket != 0:
         pixels[(purple+1)%num_leds] = (64, 0, 64)
         pixels[(purple+2)%num_leds] = (32, 0, 32)
@@ -28,6 +27,7 @@ while True:
         purple_rocket = purple_rocket - 1
     elif random.randint(0, 50) == 0:
         purple_rocket = 15
+    pixels[purple] = (128, 0, 128)
     pixels[yellow] = (128, 72, 0)
     client.put_pixels(pixels)
     time.sleep(.02)
